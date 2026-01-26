ad10e7ce819c848d8243a0a8b98315825c63eac3
KarlitoxRey <112041123+KarlitoxRey@users.noreply.github.com>
2026-01-26 08:55:32 -0300

    Update server.js

--- server.js
+++ server.js
@@ -34,6 +34,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.set('trust proxy', 1);
+
 const server = http.createServer(app);
 
 // =======================================================================
