6279555dbaf2f67f9bdd5d8073f55d2ebe502a09
KarlitoxRey <112041123+KarlitoxRey@users.noreply.github.com>
2026-02-02 14:43:26 -0300

    Update duel.routes.js

--- src/routes/duel.routes.js
+++ src/routes/duel.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { createDuel, acceptDuel, resolveDuel } from "../controllers/duel.controller.js";
-// 👇 CORRECCIÓN AQUÍ: Usamos verifyToken
+// 👇 AQUÍ ESTABA EL ERROR: Cambiamos 'protect' por 'verifyToken'
 import { verifyToken, isShogun } from "../middlewares/auth.middleware.js"; 
 
 const router = express.Router();
 
+// Usamos verifyToken en lugar de protect
 router.post("/create", verifyToken, createDuel);
 router.put("/accept/:duelId", verifyToken, acceptDuel);
 router.post("/resolve", verifyToken, isShogun, resolveDuel); 
