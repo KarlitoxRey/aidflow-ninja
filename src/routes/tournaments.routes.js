83028d0d853cc2b1e86efccaf65d389cdbe672be
KarlitoxRey <112041123+KarlitoxRey@users.noreply.github.com>
2026-02-11 10:32:43 -0300

    Update tournaments.routes.js

--- src/routes/tournaments.routes.js
+++ src/routes/tournaments.routes.js
@@ -3,9 +3,10 @@ import {
     getTournaments, 
     getRanking, 
     submitScore, 
-    createTournament 
+    createTournament,
+    deleteTournament
 } from "../controllers/tournaments.controller.js";
-import { verifyToken } from "../middlewares/auth.middleware.js"; // 👈 Corregido a 'middlewares'
+import { verifyToken } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -17,16 +18,19 @@ const router = Router();
 router.get("/", verifyToken, getTournaments);
 
 // Obtener Ranking de un torneo específico
-router.get("/ranking/:tournamentId", getRanking);
+router.get("/ranking/:tournamentId", verifyToken, getRanking);
 
 // Registrar Puntaje (Cuando termina un juego)
 router.post("/submit-score", verifyToken, submitScore);
 
 // ==========================================
-// 🛡️ RUTAS DE SHOGUN (Creación)
+// 🛡️ RUTAS DE SHOGUN (Creación / Eliminación)
 // ==========================================
 
 // Crear Torneo (La verificación de Shogun se hace dentro del controlador)
 router.post("/", verifyToken, createTournament);
+
+// Eliminar Torneo
+router.delete("/:id", verifyToken, deleteTournament);
 
 export default router;
